61bfe5a2e88a94b7acbfc7cd7cd67ffd054c0bc3
Jan Bobisud <me@bobisjan.com>
2015-03-07 14:53:17 +0100

    Use single model for contact

--- app/controllers/index.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-import config from '../config/environment';
-
-export default Ember.Controller.extend({
-  company: config.about.company
-});
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,20 +1,26 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
-var links = [{
-  title: 'GitHub',
-  url: 'https://github.com/bobisjan'
-}, {
-  title: 'Twitter',
-  url: 'https://twitter.com/bobisjan'
-}, {
-  title: 'LinkedIn',
-  url: 'https://www.linkedin.com/pub/jan-bobisud/76/802/273'
-}];
+var contact = {
+  city: config.about.city,
+  company: config.about.company,
+  gravatar: config.about.email,
+  links: [{
+    title: 'GitHub',
+    url: 'https://github.com/bobisjan'
+  }, {
+    title: 'Twitter',
+    url: 'https://twitter.com/bobisjan'
+  }, {
+    title: 'LinkedIn',
+    url: 'https://www.linkedin.com/pub/jan-bobisud/76/802/273'
+  }]
+};
 
 export default Ember.Route.extend({
 
   model: function() {
-    return links;
+    return contact;
   }
 
 });
