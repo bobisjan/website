779edfc786bdb7a692bfb789e542308df49ce6c1
Jan Bobisud <me@bobisjan.com>
2015-04-14 22:49:02 +0200

    Update to Ember CLI 0.2.3 and Ember to 1.11.1

--- tests/unit/helpers/current-year-test.js
+++ tests/unit/helpers/current-year-test.js
@@ -1,11 +1,12 @@
 import {
   currentYear
 } from 'bobisjan/helpers/current-year';
+import { module, test } from 'qunit';
 
 module('CurrentYearHelper');
 
 // Replace this with your real tests.
-test('it works', function() {
+test('it works', function(assert) {
   var result = currentYear(42);
-  ok(result);
+  assert.ok(result);
 });
