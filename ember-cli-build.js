5334096fe36d68d9c4cb157542a4b30e7aea39f6
Jan Bobisud <me@bobisjan.com>
2016-06-04 23:12:44 +0200

    Update to Ember v2.5.1

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,18 +1,17 @@
-/* global require, module */
-
+/*jshint node:true*/
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults);
+  var app = new EmberApp(defaults, { });
 
   // import bootstrap
   app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js');
   app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
   app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css.map', {
-    destDir: 'assets',
+    destDir: 'assets'
   });
   app.import(app.bowerDirectory + '/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
-    destDir: 'fonts',
+    destDir: 'fonts'
   });
 
   return app.toTree();
