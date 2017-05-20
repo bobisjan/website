eea8bc2556a02b9d4b17d392e3266bde62790f2c
Jan Bobisud <me@bobisjan.com>
2017-05-20 17:43:23 +0200

    Use SCSS for styles

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,9 +3,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    sassOptions: {
-      extension: 'sass'
-    }
+
   });
 
   return app.toTree();
