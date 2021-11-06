964dafc2b8876268a43306016f26f490b6557f40
Jan Bobisud <me@bobisjan.com>
2021-11-06 09:12:03 +0100

    Use Prettier on templates

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,7 +1,12 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended',
+  extends: [
+    'recommended',
+    'stylistic',
+    'ember-template-lint-plugin-prettier:recommended',
+  ],
+  plugins: ['ember-template-lint-plugin-prettier'],
   rules: {
     'no-curly-component-invocation': { allow: ['current-year'] },
     'no-implicit-this': { allow: ['current-year'] },
