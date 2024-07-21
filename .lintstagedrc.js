63ffc435e393f2c2c6fd7c6418393019b17e88f5
Jan Bobisud <me@bobisjan.com>
2024-07-21 21:41:16 +0200

    Run commands directly in lint staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
-  '**/*.{js,ts}': ['npx prettier --write', 'npx eslint --fix'],
-  '**/*.{gjs,gts}': ['npx prettier --write', 'npx ember-template-lint --fix'],
-  '**/*.css': ['npx prettier --write', 'npx stylelint --fix'],
-  '**/*.{html,json}': 'npx prettier --write',
+  '**/*.{js,ts}': ['prettier --write', 'eslint --fix'],
+  '**/*.{gjs,gts}': ['prettier --write', 'ember-template-lint --fix'],
+  '**/*.css': ['prettier --write', 'stylelint --fix'],
+  '**/*.{html,json}': 'prettier --write',
 };
