4cf3b8a9364ab3be5c0fb26380a37e8360dabc66
Jan Bobisud <me@bobisjan.com>
2026-04-26 17:52:13 +0200

    Rehydration

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import Application from 'ember-strict-application-resolver';
 import setupInspector from '@embroider/legacy-inspector-support/ember-source-4.12';
 import Router from './router.js';
-import { clear } from './services/fastboot.js';
+import ApplicationInstance from '@ember/application/instance';
+import { macroCondition, isTesting } from '@embroider/macros';
 
 if (import.meta.env.SSR) {
   globalThis.window = globalThis;
@@ -18,4 +19,11 @@ export default class App extends Application {
   inspector = setupInspector(this);
 }
 
-App.instanceInitializer(clear);
+ApplicationInstance.reopen({
+  _bootSync(options = {}) {
+    if (macroCondition(!isTesting())) {
+      options._renderMode = import.meta.env.SSR ? 'serialize' : 'rehydrate';
+    }
+    return this._super(options);
+  },
+});
